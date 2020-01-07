a13b9604e5d209a8e76f949076c4a830de49bd8d
Taha Erden <tahaerden@windowslive.com>
2020-01-07 17:09:52 +0300

    Removed un-used toastOptions model.

--- src/app/models/toastOptions.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ToastOptions } from './toastOptions';
-
-describe('ToastOptions', () => {
-  it('should create an instance', () => {
-    expect(new ToastOptions()).toBeTruthy();
-  });
-});
