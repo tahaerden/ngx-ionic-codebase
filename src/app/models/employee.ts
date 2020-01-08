ac5d8aab1e261475b0ba6ccc867337c73bf6b8a6
Taha Erden <tahaerden@windowslive.com>
2020-01-08 11:56:31 +0300

    Added employee avatar to employee-details.

--- src/app/models/employee.ts
+++ src/app/models/employee.ts
@@ -3,6 +3,7 @@ export interface IEmployee {
   name: string;
   salary: string;
   age: string;
+  avatar: string;
 }
 
 export class Employee implements IEmployee {
@@ -10,11 +11,13 @@ export class Employee implements IEmployee {
   public name: string;
   public salary: string;
   public age: string;
+  public avatar: string;
 
   constructor(data: any) {
     this.id = data.id;
     this.name = data.employee_name;
     this.salary = data.employee_salary;
     this.age = data.employee_age;
+    this.avatar = data.profile_image;
   }
 }
