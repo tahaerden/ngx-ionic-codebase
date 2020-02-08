8311fce136c534f0d3e8c6b408d26b66243c02e4
Taha Erden <tahaerden@windowslive.com>
2020-02-08 13:45:52 +0300

    Added base-href option to build script. Added placeholders to login inputs. Changed prod local apiUrl.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: {
-    local: 'http://localhost:4200',
+    local: 'http://tahaerden.com/ionic',
     employeeApi: 'https://dummy.restapiexample.com/api/v1',
     instagramApi: 'https://n161.tech/api/dummyapi'
   }
